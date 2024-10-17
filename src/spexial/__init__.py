04a0e562285fd91fa436b9f2e8ee4b556840060d
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-10-17 19:41:50 -0400

    refactor: match Gegenbauer to scipy.special (#8)
    * ci: ignore mypy_cache
    * refactor: match gegenbauer to scipy
    * test: test gegenbauer
    * build: add jaxtyping
    
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/spexial/__init__.py
+++ src/spexial/__init__.py
@@ -1,6 +1,10 @@
 """Special functions."""
 
-__all__ = ["gegenbauer_polynomials"]
+__all__ = [
+    # Gegenbauer
+    "eval_gegenbauer",
+    "eval_gegenbauers",  # NOTE: not in scipy.special
+]
 
-from ._src.gegenbauer import gegenbauer_polynomials
+from ._src.gegenbauer import eval_gegenbauer, eval_gegenbauers
 from ._version import version as __version__  # noqa: F401
