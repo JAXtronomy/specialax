f7b603848848979957d57f020fe301e971fa924a
Cara Giovanetti <caragiovanetti@gmail.com>
2024-10-17 11:47:42 -0400

    added LINX fns to src

---
+++ src/spexial/__init__.py
@@ -0,0 +1,10 @@
+"""Special functions."""
+
+__all__ = ["gegenbauer_polynomials"]
+
+from ._src.gegenbauer import gegenbauer_polynomials
+from ._src.comb import comb
+from ._src.gamma import gamma
+from ._src.kn import K0,K1,K2
+from ._src.polylog import Li
+from ._src.zeta import Riemann_zeta as zeta
