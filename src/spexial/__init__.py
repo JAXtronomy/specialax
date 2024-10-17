3fde2f6435b5282548b9cf9dd606a684dad6b34a
nstarman <nstarman@users.noreply.github.com>
2024-10-17 18:09:08 -0400

    refactor: improve import annotation
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/spexial/__init__.py
+++ src/spexial/__init__.py
@@ -1,6 +1,10 @@
 """Special functions."""
 
-__all__ = ["eval_gegenbauer", "eval_gegenbauers"]
+__all__ = [
+    # Gegenbauer
+    "eval_gegenbauer",
+    "eval_gegenbauers",  # NOTE: not in scipy.special
+]
 
 from ._src.gegenbauer import eval_gegenbauer, eval_gegenbauers
 from ._version import version as __version__  # noqa: F401
