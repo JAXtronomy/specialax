9c48cc3ac9e3c444c6b5fe4f3ee09f39946f0731
Cara Giovanetti <caragiovanetti@gmail.com>
2024-10-17 17:20:14 -0400

    git stuff

--- src/spexial/__init__.py
+++ src/spexial/__init__.py
@@ -3,9 +3,4 @@
 __all__ = ["gegenbauer_polynomials"]
 
 from ._src.gegenbauer import gegenbauer_polynomials
-from ._src.comb import comb
-from ._src.gamma import gamma
-from ._src.kn import K0,K1,K2
-from ._src.polylog import Li
-from ._src.zeta import Riemann_zeta as zeta
-
+from ._version import version as __version__  # noqa: F401
