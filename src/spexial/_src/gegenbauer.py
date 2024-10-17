ce8158aec30c54af3bd3f9318b0e34633f8995d1
nstarman <nstarman@users.noreply.github.com>
2024-10-17 15:51:01 -0400

    build: set up ci
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/spexial/_src/gegenbauer.py
+++ src/spexial/_src/gegenbauer.py
@@ -1,31 +1,26 @@
 """Jax implementation of the Gegenbauer polynomials."""
 
-import jax.numpy as jnp
 from functools import partial
+
 import jax
+import jax.numpy as jnp
 
 
 @jax.jit
 def C0(x):
-    """
-    Initialize with C0
-    """
+    """Initialize with C0"""
     return 1.0
 
 
 @jax.jit
 def C1(x, alpha):
-    """
-    Initialize with C1
-    """
+    """Initialize with C1"""
     return 2 * alpha * x
 
 
 @jax.jit
 def C_n_plus_1(carry, n):
-    """
-    Function to be used in scan
-    """
+    """Function to be used in scan"""
     x, alpha, Cn, Cn_minus_1 = carry
     accumulate = (2 * (n + alpha) * x * Cn - (n + 2 * alpha - 1) * Cn_minus_1) / (n + 1)
     carryover = (x, alpha, accumulate, Cn)
@@ -35,8 +30,7 @@ def C_n_plus_1(carry, n):
 
 @partial(jax.jit, static_argnums=(2,))
 def gegenbauer_polynomials(x=None, alpha=None, N=None):
-    """
-    Implements scan of C_n_plus_1
+    """Implements scan of C_n_plus_1
     Returns every order of the polynomial computed along the way
     """
     C0_val = C0(x)
@@ -44,7 +38,8 @@ def gegenbauer_polynomials(x=None, alpha=None, N=None):
 
     carry = (x, alpha, C1_val, C0_val)
     n_values = jnp.arange(
-        1, N
+        1,
+        N,
     )  # n starts from 1 since we already initialized 0->1 above
 
     _, C_values = jax.lax.scan(C_n_plus_1, carry, n_values)
@@ -54,8 +49,7 @@ def gegenbauer_polynomials(x=None, alpha=None, N=None):
 
 @partial(jax.jit, static_argnums=(2,))
 def C_n_alpha(x=None, alpha=None, N=None):
-    """
-    Vmapped version of gegenbauer_polynomials,
+    """Vmapped version of gegenbauer_polynomials,
     assumes batched x inputs
     """
     return jax.vmap(gegenbauer_polynomials, in_axes=(0, None, None))(x, alpha, N)[:, -1]
