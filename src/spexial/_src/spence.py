7391acfb7d0b00a629b496c4ecd94130d6167697
Colm Talbot <talbotcolm@gmail.com>
2024-10-21 16:33:34 +0000

    TEST: add tests and some more documentation

--- src/spexial/_src/spence.py
+++ src/spexial/_src/spence.py
@@ -13,11 +13,14 @@ import numpy as np
 MAXITER = 500
 
 
-@jax.numpy.vectorize
+@jnp.vectorize
 def complex_spence_series_0(z):
-    """
+    r"""
     Small z branch, uses a series expansion about :math:`z = 0`
     for :math:`|z| < 0.5`.
+
+    The third term diverges for :math:`|z| \rightarrow 0` so we special case
+    it.
     """
     
     nn = jnp.arange(1, MAXITER)
@@ -25,10 +28,14 @@ def complex_spence_series_0(z):
     sum1 = jnp.sum(temp / nn)
     sum2 = jnp.sum(temp)
     
-    return np.pi**2 / 6 - sum1 + jnp.log(z) * sum2
+    return jnp.where(
+        abs(z) < 1e-15,
+        np.pi**2 / 6,
+        np.pi**2 / 6 - sum1 + jnp.log(z) * sum2
+    )
 
 
-@jax.numpy.vectorize
+@jnp.vectorize
 def complex_spence_series_1(z):
     """
     Middle branch, an expansion around :math:`z = 1`
@@ -48,19 +55,33 @@ def complex_spence_series_2(z):
     """
     Large :math:`z` branch for :math:`|z| > 0.5` and
     :math:`|1 - z| > 1`. Uses a reflection expression.
+
+    This gives a :code:`ZeroDivisionError` for `z = 1`. Since every branch is
+    evaluated when this has been :code:`vmap`-ed, we perturb the value
+    slightly.
     """
+    val = jnp.where(z == 1, z, z + 1e-10)
     return (
-        -complex_spence_series_1(z / (z - 1))
+        -complex_spence_series_1(val / (val - 1))
         - np.pi**2 / 6
-        - jnp.log(z - 1)**2 / 2
+        - jnp.log(val - 1)**2 / 2
     )
 
 
 @jax.jit
 def spence(z):
-    """
+    r"""
     Return the Spence dilogarithm for complex input using
     branches dependent on the value of the input.
+
+    .. math::
+
+        \int_{1}^{z} dt \frac{\log(t)}{1 - t}
+
+    See Also
+    --------
+    scipy.special.spence: the reference implementation in scipy
+    jax.scipy.special.spence: jax implementation for real inputs
     """
     return jax.lax.select(
         abs(z) < 0.5,
@@ -73,11 +94,30 @@ def spence(z):
     )
 
 
+def spence_gradient(z):
+    r"""
+    Analytic gradient of the spence dilogarithm. Since :code:`spence` is
+    defined via an integral, this is just the integrand
+
+    .. math::
+
+        \frac{\log(z)}{1 - z}
+
+    We call out special cases where one of the two terms will diverge.
+    """
+    return jnp.where(
+        (z == 0) | (z == 1),
+        0.0,
+        jnp.log(z) / (1 - z),
+    )
+
+
 def spence_jvp(primals, tangents):
     """Template of spence jvp that different implementations can use"""
     (z,) = primals
     (z_dot,) = tangents
-    dspence = jnp.log(z) / (1 - z)
+    dspence = spence_gradient(z)
+    jax.debug.print("{} {}", dspence, z_dot)
     return spence(z), z_dot * dspence
 
 
